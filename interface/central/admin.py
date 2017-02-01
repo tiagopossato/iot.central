3e63b4173eae37a908316046acb1080f2387b04a
Tiago Possato <tiago@debian>
2017-02-01 13:27:27 -0200

    adicionado atributo list_per_page em todas as classes

--- interface/central/admin.py
+++ interface/central/admin.py
@@ -11,6 +11,7 @@ class LogAdmin(admin.ModelAdmin):
     list_display = ('tipo','mensagem','tempo',)
     ordering = ('-tempo',)
     list_filter = ('tipo',)
+    list_per_page = 50
 
 class AlarmeAdmin(admin.ModelAdmin):
     readonly_fields = ('ativo','alarmeTipo','tempoAtivacao','syncAtivacao','tempoInativacao','syncInativacao',)
@@ -22,14 +23,17 @@ class AlarmeAdmin(admin.ModelAdmin):
 class AlarmeTipoAdmin(admin.ModelAdmin):
     list_display = ('codigo','mensagem','prioridade',)
     ordering = ('codigo',)
+    list_per_page = 50
 
 class PlacaExpansaoDigitalAdmin(admin.ModelAdmin):
     list_display = ('descricao','idRede',)
     ordering = ('idRede',)
+    list_per_page = 50
 
 class EntradaDigitalAdmin(admin.ModelAdmin):
     list_display = ('placaExpansaoDigital','numero','nome','estado','alarmeTipo',)
     ordering = ('placaExpansaoDigital', 'numero',)
+    list_per_page = 50
 
 admin.site.register(Log, LogAdmin)
 admin.site.register(AlarmeTipo, AlarmeTipoAdmin)
