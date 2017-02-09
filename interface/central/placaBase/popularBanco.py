c288aad0683ffadefe02367f06d0d0a0352a3d29
Tiago Possato <tiago.possato@yahoo.com.br>
2017-02-09 10:19:34 -0200

    Alterado popular Banco

--- interface/central/placaBase/popularBanco.py
+++ interface/central/placaBase/popularBanco.py
@@ -3,7 +3,7 @@ import os
 import sys
 import django
 
-sys.path.insert(0, os.path.abspath('../../interface'))
+sys.path.insert(0, os.path.abspath('../../../interface'))
 os.environ["DJANGO_SETTINGS_MODULE"] = "interface.settings"
 django.setup()
 
