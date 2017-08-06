130415e3cd8317e9b0d2b6f161ee9a34083a9ec3
Tiago Possato <tiago@debian>
2017-08-06 19:20:32 -0300

    limpando versao

--- interface_velha/central/static/js/init.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    //retrai o menu, somente em telas largas
-    if (window.matchMedia('(min-width: 992px)').matches) {
-        $('#menu_toggle').click();
-    }
-});
--- interface_velha/central/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib.auth import views as auth_views
-from . import views
-
-urlpatterns = [
-    url(r'^login', views.login_view),
-    url(r'^logout', views.logout_view),
-    url(r'^$', views.index),
-]
