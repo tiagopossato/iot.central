ef1cfb95ac35284a9dc102f34b5f6b6c183f66ca
Tiago Possato <tiago.possato@yahoo.com.br>
2017-09-23 15:27:08 -0300

    Alterado grafico para Plotly

--- central/aplicacao/static/js/grafico.js
+++ central/aplicacao/static/js/grafico.js
@@ -1,77 +1,34 @@
 var initGrafico = function () {
-    var myChart = echarts.init(document.getElementById('grafico'));
-    // use configuration item and data specified to show chart
-    var option = {
-        title: {
-            text: 'Ultimas 12 horas'
+    var data = [];
+    var layout = {
+        legend:{
+            orientation: 'h',
+            yanchor: 'top',
+            xanchor: 'left'
         },
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            data: []
-        },
-        // toolbox: {
-        //     show: true,
-        //     feature: {
-        //         dataZoom: {
-        //             yAxisIndex: 'none',
-        //             name: 'testes'
-        //         },
-        //         dataView: { readOnly: false },
-        //         magicType: { type: ['line', 'bar'] },
-        //         restore: {},
-        //         saveAsImage: {}
-        //     }
-        // },
-        xAxis: {
-            type: 'category',
-            boundaryGap: false,
-            data: []
-        },
-        yAxis: {
-            type: 'value',
-            axisLabel: {
-                formatter: '{value} °C'
-            }
-        },
-        series: []
+        showlegend: true
     };
 
-    myChart.showLoading();
+
     $.get(location.protocol + "//" + location.host + "/app/leituras", function (data, status) {
-        console.log(data);
         data.forEach(function (sensor) {
             // console.log(sensor)
             sensor.g.forEach(function (grandeza) {
-                linha = {
-                    name: sensor.d,
-                    type: 'line',
-                    data: [],
-                    markPoint: {
-                        data: [
-                            { type: 'max', name: 'Max' },
-                            { type: 'min', name: 'Max' }
-                        ]
-                    },
-                    markLine: {
-                        data: [
-                            { type: 'average', name: 'Média' }
-                        ]
-                    }
+                var trace = {
+                    x: [],
+                    y: [],
+                    type: 'scatter',
+                    name: sensor.d + ' [' + grandeza.n + ']'
                 }
-                grandeza.l.forEach(function(leitura){
-                    linha.data.push(leitura.v);
-                },this);
-                
-                // console.log(linha)
-                option.legend.data.push(sensor.d+'[' + grandeza.n + ']')
-                option.series.push(linha);
+                grandeza.l.forEach(function (leitura) {
+                    // console.log(leitura)
+                    trace.y.push(leitura.v);
+                    trace.x.push(leitura.c);
+                }, this);
+                data.push(trace);
             }, this);
         }, this);
-        console.log(option);
-        myChart.setOption(option);
-        myChart.hideLoading();
+        Plotly.newPlot('grafico', data, layout);
     });
 
-};
+}
