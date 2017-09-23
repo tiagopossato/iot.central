173c88f4c4f6e38afbbdd3a2f5c88cb73a8d3602
Tiago Possato <tiago.possato@yahoo.com.br>
2017-09-23 15:37:57 -0300

    Adicionada mensagem ao carregar os dados

--- central/aplicacao/static/js/grafico.js
+++ central/aplicacao/static/js/grafico.js
@@ -1,7 +1,7 @@
 var initGrafico = function () {
     var data = [];
     var layout = {
-        legend:{
+        legend: {
             orientation: 'h',
             yanchor: 'top',
             xanchor: 'left'
@@ -9,8 +9,14 @@ var initGrafico = function () {
         showlegend: true
     };
 
+    var dialog = bootbox.dialog({
+        message: '<p class="text-center"><i class="fa fa-spin fa-spinner"></i></p><p class="text-center">Buscando dados...</p>',
+        closeButton: false,
+        size: 'small'
+    });
 
     $.get(location.protocol + "//" + location.host + "/app/leituras", function (data, status) {
+
         data.forEach(function (sensor) {
             // console.log(sensor)
             sensor.g.forEach(function (grandeza) {
@@ -28,6 +34,7 @@ var initGrafico = function () {
                 data.push(trace);
             }, this);
         }, this);
+        dialog.modal('hide');
         Plotly.newPlot('grafico', data, layout);
     });
 
